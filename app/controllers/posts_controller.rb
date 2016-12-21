2b25770ef09f8f573a160b7012a8afaf40aac6f8
Awesome2K <qShadowp@gmail.com>
2016-12-21 19:54:34 +0200

    Added basic post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,34 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params) do |t|
+      if params[:post][:data]
+        t.attachment = params[:post][:data].read
+        t.filename = params[:post][:data].original_filename
+        # TODO
+        t.mime = 1
+      end
+    end
+
+    if @post.save
+      redirect_to(@post, :notice => 'Post was created')
+    else
+      render :action => "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:comment, :post)
+  end
 end
